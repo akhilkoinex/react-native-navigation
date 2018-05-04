3ee2b31705b3567240a78d66040aa77644d7d1cc
Akhil James <akhiljames@live.com>
2018-05-04 12:41:31 +0530

    clearHostOnActivityDestroy

--- android/app/src/main/java/com/reactnativenavigation/NavigationApplication.java
+++ android/app/src/main/java/com/reactnativenavigation/NavigationApplication.java
@@ -128,7 +128,7 @@ public abstract class NavigationApplication extends Application implements React
     public abstract boolean isDebug();
 
     public boolean clearHostOnActivityDestroy(Activity activity) {
-        return true;
+        return false;
     }
 
     @Nullable
